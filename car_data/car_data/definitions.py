ab29e724fc7d301361a24ecb63e09b695ee026be
jhonny <ramirezjhonny64@gmail.com>
2025-08-25 16:36:38 -0500

    Add assets to all_assets loading in definitions.py

--- car_data/car_data/definitions.py
+++ car_data/car_data/definitions.py
@@ -4,7 +4,7 @@ from car_data import assets # noqa: TID252
 from . import activos_filtros
 from . import count_cars
 
-all_assets = load_assets_from_modules([count_cars])
+all_assets = load_assets_from_modules([assets,activos_filtros,count_cars])
 
 defs = Definitions(
     assets=all_assets
